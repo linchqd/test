b28c61767bfa078e4d30abdace0e33a03b28ad61
liuld <liuldgm@gmain.com>
2020-03-18 00:08:18 +0800

    gate

--- gateio/client.py
+++ gateio/client.py
@@ -2,30 +2,61 @@
 # -*- coding: utf-8 -*-
 # encoding: utf-8
 
-from util import GateApi
+
+import logging
+from util import GateApi, Redis, compute_order_info
+import sys
 from pprint import pprint
 
-
-apiKey = 'E23B5533-1993-488B-B161-64899858EEE3'
+# api info
+apiKey = 'E23B5533-1995-488B-B161-64899858EEE3'
 secretKey = 'df41f014eeabffde49d2d4b766b9263f42d59bcf0deb8a4fcdd5f8d849af837b'
 
 API_QUERY_URL = 'data.gateio.life'
 API_TRADE_URL = 'api.gateio.life'
+REDIS_HOST = '192.168.10.20'
 
+
+# init obj
 gate_query = GateApi(API_QUERY_URL, apiKey, secretKey)
 gate_trade = GateApi(API_TRADE_URL, apiKey, secretKey)
+redis = Redis(host=REDIS_HOST)
 
-# 获取最新成交价格
-last_price = gate_query.ticker('ae_usdt')['last']
+# set run var
+AMOUNT = 100
+PERCENT = 0.01
+CURRENCY_PAIR = 'ae_usdt'
 
-base_price = 0
 
-current_counts = 0
+logging.info('程序开始启动.....')
+
+if not redis.init_price:
+    try:
+        # 获取最新成交价格
+        redis.init_price = gate_query.ticker('ae_usdt')['last']
+        logging.info('程序第一次启动,获取init price为最新成交价: {}'.format(redis.init_price))
+        redis.number = 0
+        redis.amount = AMOUNT
+        redis.percent = PERCENT
+        order_info = compute_order_info(redis.init_price, redis.number, redis.amount, redis.percent)
+        gate_trade.buy(CURRENCY_PAIR, order_info['buy_order_price'], order_info['buy_order_amount'])
+        logging.info('挂买单, 买入价格: {}, 数量: {}'.format(order_info['buy_order_price'], order_info['buy_order_amount']))
+        gate_trade.sell(CURRENCY_PAIR, order_info['sell_order_price'], order_info['sell_order_amount'])
+        logging.info('挂卖单, 卖出价格: {}, 数量: {}'.format(order_info['sell_order_price'], order_info['sell_order_amount']))
+
+    except Exception as e:
+        logging.error('程序发生错误,错误原因: {}'.format(str(e.args)))
+        logging.error('程序退出')
+        sys.exit(1)
+
+
 
 current_order = {
-    'price': 0,
-    'amount': 0
+    'price': 10.1,
+    'amount': 30
 }
+# redis.conn.hmset('current_order', current_order)
+# print(redis.conn.hmget('current_order', ['price', 'amount'])[0].decode('utf-8'))
 # 当前的买和卖挂单信息   买和卖的总数量
 '''
     A.程序启动 判断是否有没有 衡量价格  
@@ -51,6 +82,8 @@ current_order = {
             部分成交:
     https://blog.csdn.net/linwow/article/details/95227392
 '''
-pprint(gate_trade.get_order({'orderNumber': 9019936601, 'currencyPair': 'cdt_usdt'}))
+# pprint(gate_trade.get_order({'orderNumber': 9019936601, 'currencyPair': 'cdt_usdt'}))
 
 # pprint(gate_trade.all_orders({'currencyPair': 'cdt_usdt'}))
+# print(int(redis.amount))
+redis.flush_all_keys()
--- gateio/util.py
+++ gateio/util.py
@@ -7,6 +7,72 @@ import hashlib
 import urllib.parse
 import hmac
 import json
+import ssl
+import os
+import logging
+from logging.config import dictConfig
+
+from redis import StrictRedis, ConnectionPool
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+# logging setting
+class LogFilter(logging.Filter):
+    def filter(self, record):
+        if record.levelno > 30:
+            return False
+        return True
+
+
+log_dir = os.path.join(os.path.dirname(os.path.abspath(__name__)), 'logs/')
+
+os.makedirs(log_dir, exist_ok=True)
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'access': {
+            'format': '%(asctime)s %(levelname)s: %(message)s'
+        },
+        'error': {
+            'format': '%(asctime)s %(levelname)s: %(filename)s:%(module)s:%(funcName)s:%(lineno)d:%(message)s'
+        }
+    },
+    'handlers': {
+        'access': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': 'INFO',
+            'filename': '{}access.log'.format(log_dir),
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 7,
+            'encoding': 'utf-8',
+            'formatter': 'access',
+            'filters': ['access']
+        },
+        'error': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': 'ERROR',
+            'filename': '{}error.log'.format(log_dir),
+            'when': 'D',
+            'interval': 1,
+            'backupCount': 7,
+            'encoding': 'utf-8',
+            'formatter': 'error'
+        }
+    },
+    'filters': {
+        'access': {
+            '()': LogFilter
+        }
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['access', 'error']
+    }
+})
 
 
 def get_sign(params, secret_key):
@@ -62,6 +128,12 @@ class GateApi(object):
         param = {}
         return http_post(self.__url, url, param, self.__apiKey, self.__secretKey)
 
+    # 市场订单参数
+    def market_info(self):
+        url = "/api2/1/marketinfo"
+        params = ''
+        return http_get(self.__url, url, params)
+
     # 所有交易对
     def pairs(self):
         url = "/api2/1/pairs"
@@ -73,6 +145,18 @@ class GateApi(object):
         url = "/api2/1/ticker"
         return http_get(self.__url, url, param)
 
+    # 买入
+    def buy(self, currency_pair, rate, amount):
+        url = "/api2/1/private/buy"
+        params = {'currencyPair': currency_pair, 'rate': rate, 'amount': amount}
+        return http_post(self.__url, url, params, self.__apiKey, self.__secretKey)
+
+    # 卖出
+    def sell(self, currency_pair, rate, amount):
+        url = "/api2/1/private/sell"
+        params = {'currencyPair': currency_pair, 'rate': rate, 'amount': amount}
+        return http_post(self.__url, url, params, self.__apiKey, self.__secretKey)
+
     # 获取我的当前挂单列表
     def all_orders(self, params):
         url = "/api2/1/private/openOrders"
@@ -83,3 +167,70 @@ class GateApi(object):
     def get_order(self, params):
         url = "/api2/1/private/getOrder"
         return http_post(self.__url, url, params, self.__apiKey, self.__secretKey)
+
+
+class Redis(object):
+    def __init__(self, host='127.0.0.1', port=6379, db=0, password=None):
+        self.conn = StrictRedis(connection_pool=ConnectionPool(
+            host=host, port=port, db=db, password=password, decode_responses=True))
+
+    @property
+    def init_price(self):
+        return self.conn.get('init_price')
+
+    @init_price.setter
+    def init_price(self, value):
+        self.conn.set('init_price', value)
+
+    @property
+    def number(self):
+        return self.conn.get('number')
+
+    @number.setter
+    def number(self, value):
+        self.conn.set('number', value)
+
+    @property
+    def amount(self):
+        return self.conn.get('amount')
+
+    @amount.setter
+    def amount(self, value):
+        self.conn.set('amount', value)
+
+    @property
+    def percent(self):
+        return self.conn.get('percent')
+
+    @percent.setter
+    def percent(self, value):
+        self.conn.set('percent', value)
+
+    def flush_all_keys(self):
+        for key in self.conn.keys():
+            self.conn.delete(key)
+
+
+def compute_order_info(init_price, number, amount, percent, buy=False):
+    if int(number) == 0:
+        buy_order_price = '%.4f' % (float(init_price) * (1 - float(percent)))
+        buy_order_amount = '%.3f' % (float(amount) * (2 ** int(number)))
+        sell_order_price = '%.4f' % (float(init_price) * (1 + float(percent)))
+        sell_order_amount = '%.3f' % (float(amount) * (2 ** int(number)))
+    elif buy:
+        buy_order_price = '%.4f' % (float(init_price) * (1 - (float(percent) * (int(number) + 1))))
+        buy_order_amount = '%.3f' % (float(amount) * (2 ** int(number)))
+        sell_order_price = '%.4f' % (float(init_price) * (1 - (int(number) - 1) * float(percent)))
+        sell_order_amount = '%.3f' % (float(buy_order_amount * 0.998))
+    else:
+        sell_order_price = '%.4f' % (float(init_price) * (1 + (float(percent) * (int(number) + 1))))
+        sell_order_amount = '%.3f' % (float(amount) * (2 ** int(number)))
+        buy_order_price = '%.4f' % (float(init_price) * (1 + (int(number) - 1) * float(percent)))
+        buy_order_amount = '%.3f' % (sell_order_amount * 1.002)
+
+    return {
+        'buy_order_price': buy_order_price,
+        'buy_order_amount': buy_order_amount,
+        'sell_order_price': sell_order_price,
+        'sell_order_amount': sell_order_amount
+    }
